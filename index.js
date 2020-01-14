f70bff42fe4b54e986ca9eba1c307d1d46b16605
CYRUS <cyruspassi@gmail.com>
2020-01-15 02:08:17 +0530

    include script ans styles

--- index.js
+++ index.js
@@ -4,9 +4,16 @@ const port=8000;
 
 const expressLayouts=require('express-ejs-layouts');
 
+app.use(express.static('./assets'));  //including css js and images files
+
+
 
 app.use(expressLayouts);
 
+
+//extract style and scripts from subpages into layput
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 //setting routes                       
 app.use('/',require('./routes'));            //for commiting git add . then git commit -m '   '
 
