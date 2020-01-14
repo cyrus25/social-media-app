a9c6ee63f33860bd82c165fddb3a73967dd365c1
CYRUS <cyruspassi@gmail.com>
2020-01-14 23:36:23 +0530

    setting view engine

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//setting routes
 app.use('/',require('./routes'));
+
+//setting view engine
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err)
     {
