26bda32c38132bfe3ae13fc502be0fc452a0d009
CYRUS <cyruspassi@gmail.com>
2020-03-11 03:06:56 +0530

    added googleoauth strategy

--- routes/users.js
+++ routes/users.js
@@ -30,5 +30,9 @@ router.get('/sign-out',usersController.destroySession);
     })(req, res, next);
     });*/
 
+
+    router.get('/auth/google', passport.authenticate('google', {scope: ['profile', 'email']}));
+router.get('/auth/google/callback', passport.authenticate('google', {failureRedirect: '/users/sign-in'}), usersController.createSession);
+
  
 module.exports = router;
