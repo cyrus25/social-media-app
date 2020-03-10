c6da694c9d4f190ff26cc5fbb2366a32e4227213
CYRUS <cyruspassi@gmail.com>
2020-03-10 22:27:53 +0530

    added tokens

--- config/passport-jwt-strategy.js
+++ config/passport-jwt-strategy.js
@@ -1,1 +1,28 @@
-const passport=require('passport');
+const passport = require('passport');
+const JWTStrategy = require('passport-jwt').Strategy;
+const ExtractJWT = require('passport-jwt').ExtractJwt;  //extracr jwt from header
+
+const User = require('../models/user');
+
+
+let opts = {
+    jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken,
+    secretOrKey: 'codeial'
+}
+
+
+passport.use(new JWTStrategy(opts, function(jwtPayLoad, done){   
+
+    User.findById(jwtPayLoad._id, function(err, user){   //jwtPayload contains user info
+        if (err){console.log('Error in finding user from JWT'); return;}
+
+        if (user){
+            return done(null, user);
+        }else{
+            return done(null, false);
+        }
+    })
+
+}));
+
+module.exports = passport;
